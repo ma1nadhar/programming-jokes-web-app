a504efef5abdf29b6703492f061503498c45b812
Maina Dhar <mdhar2024@gmail.com>
2023-07-10 18:48:12 -0700

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,17 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke() { 
+            
+
+        
+        }
+
+
+    }
+}
